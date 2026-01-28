408d0c4d0bcd4d4328787e58ab9a5bb02f1e7df4
Luis J. Centeno <luisjcenteno@live.com>
2026-01-28 10:52:44 -0600

    fix(database): remove connection URL construction in favor of DATABASE_URL

--- src/database/index.js
+++ src/database/index.js
@@ -3,13 +3,9 @@ import seed from './seed'
 import { runMigrations } from './migrations'
 
 // Simple connection string built from existing Postgres env vars
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } = process.env
+const { DATABASE_URL } = process.env
 
-const connectionUrl = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/${POSTGRES_DB}`
-
-console.debug('Connecting to database at', connectionUrl)
-
-export const database = new SQL(connectionUrl)
+export const database = new SQL(DATABASE_URL)
 
 export const init = async () => {
     console.log('Database initialized')
